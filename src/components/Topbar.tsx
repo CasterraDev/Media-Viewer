fcf572466f41e35baf93ea6353b86d05c07673ea
CasterraDev <casterradev@gmail.com>
2025-09-27 18:07:03 -0400

    Fix: Sidebar now close on nav change

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import {
     Sheet,
     SheetClose,
     SheetContent,
-    SheetDescription,
-    SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
@@ -15,7 +12,6 @@ import Link from "next/link";
 import Search from "./Search";
 
 export default function Topbar() {
-
     return (
         <div className="p-3 sticky top-0 z-50 bg-[var(--color-background)] flex flex-row gap-5">
             <Sheet>
@@ -24,17 +20,22 @@ export default function Topbar() {
                         <GiHamburgerMenu className="w-auto h-full" />
                     </Button>
                 </SheetTrigger>
-                <SheetContent side="left" className="w-fit p-5">
-                    <SheetHeader>
+                <SheetContent side="left" className="w-fit p-5 gap-3">
+                    <SheetHeader className="py-1 mt-3">
                         <SheetTitle>Where to go</SheetTitle>
                     </SheetHeader>
-                    <div className="grid flex-1 auto-rows-min gap-3 px-4">
-                        <Link href="/">
-                            Home
-                        </Link>
-                        <Link href="/settings">
-                            Settings
-                        </Link>
+                    <hr />
+                    <div className="grid flex-1 auto-rows-min gap-3 px-4 *:text-[var(--color-foreground)] *:font-bold *:uppercase">
+                        <SheetClose asChild>
+                            <Link href="/">
+                                Home
+                            </Link>
+                        </SheetClose>
+                        <SheetClose asChild>
+                            <Link href="/settings">
+                                Settings
+                            </Link>
+                        </SheetClose>
                     </div>
                 </SheetContent>
             </Sheet>
