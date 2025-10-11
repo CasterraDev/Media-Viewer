7d07701446e516090791fc3b5fee11a3fe4e2ea6
CasterraDev <casterradev@gmail.com>
2025-10-11 00:10:31 -0400

    Fix: Styling & Disabled during upload

--- src/components/UploadBtn.tsx
+++ src/components/UploadBtn.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import useUserPrefs from "@/hooks/useUserPrefs";
-import { MouseEventHandler, useEffect, useState } from "react"
+import { useState } from "react"
 import { IoIosArrowDown } from "react-icons/io";
+import { Button } from "./ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -11,40 +11,41 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { UserPrefs } from "@/_types/type";
 
-export default function UploadBtn({roots}: {roots: string[]}) {
+export default function UploadBtn({ roots }: { roots: string[] }) {
     const [uploading, setUploading] = useState<boolean>(false)
 
     const click = async (_event: any, rootsList: string[] = roots) => {
         setUploading(true);
-        console.log("Uploading dir")
-        console.log(rootsList);
         const body = {
             mediaRoots: rootsList
         }
 
-        const res = await fetch("/api/uploadFilesToDb", {
+        await fetch("/api/uploadFilesToDb", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(body)
+        }).finally(() => {
+            setUploading(false);
         })
-        setUploading(false);
     }
 
     return (
-        <div className="w-full h-full flex flex-row border-[1.5px] border-[var(--color-foreground)] rounded-lg">
-            <button disabled={uploading} onClick={(e) => click(e)} className=" rounded-bl-lg rounded-tl-lg cursor-pointer border-white p-2 hover:bg-[var(--color-foreground)] hover:text-[var(--color-background)]">Upload</button>
-            <div className="h-auto w-0.5 py-1 bg-[var(--color-foreground)]" />
+        <div className="w-full h-full flex flex-row border-2 border-[var(--color-foreground)] rounded-lg">
+            <Button disabled={uploading} onClick={(e) => click(e)}
+                className="rounded-br-none rounded-tr-none">
+                Upload
+            </Button>
+            <div className="h-auto w-0.5 py-1 bg-accent" />
             <DropdownMenu>
-                <DropdownMenuTrigger disabled={uploading} className="hover:bg-[var(--color-foreground)] hover:text-[var(--color-background)] rounded-br-lg rounded-tr-lg"><IoIosArrowDown /></DropdownMenuTrigger>
+                <DropdownMenuTrigger disabled={uploading} className="bg-primary text-primary-foreground rounded-br-md rounded-tr-md hover:bg-primary/90"><IoIosArrowDown /></DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuLabel className="capitalize font-bold">Roots</DropdownMenuLabel>
                     <DropdownMenuSeparator />
                     {roots.map((r, i) => (
-                        <DropdownMenuItem key={i} className="cursor-pointer" onClick={(e) => click(e, [r])}>{r}</DropdownMenuItem>
+                        <DropdownMenuItem key={i} disabled={uploading} className="cursor-pointer" onClick={(e) => click(e, [r])}>{r}</DropdownMenuItem>
                     ))}
                 </DropdownMenuContent>
             </DropdownMenu>
